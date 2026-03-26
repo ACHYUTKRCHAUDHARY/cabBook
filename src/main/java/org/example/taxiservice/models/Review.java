17ced7288491e93845e51845d504955db92f72f4
achyut <achyutchaudhary47@gmail.com>
2026-03-26 07:53:18 +0530

    configuration settings

---
+++ src/main/java/org/example/taxiservice/models/Review.java
@@ -0,0 +1,4 @@
+package org.example.taxiservice.models;
+
+public class Review {
+}
---
+++ src/test/java/org/example/taxiservice/TaxiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.taxiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
