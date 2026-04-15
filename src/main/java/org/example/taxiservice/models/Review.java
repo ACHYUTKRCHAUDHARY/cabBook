8851f1290ef55a5082be76a4218bc4fd2d225508
achyut <achyutchaudhary47@gmail.com>
2026-04-15 20:03:12 +0530

    db model

--- src/main/java/org/example/taxiservice/models/Review.java
+++ src/main/java/org/example/taxiservice/models/Review.java
@@ -1,6 +1,10 @@
 package org.example.taxiservice.models;
 
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "bookingReview")
@@ -16,4 +20,16 @@ public class Review {
     String content;
 
     Double ratings;
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)//this annotations tells spring about
+    // the formate of date object to bestored i.e date/time?timestamp
+    @CreatedDate // this annotation tells spring that only handle it for the
+    // object creation
+    Date createAt;
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate// this annotations tells spring that only handle it for
+    // object update
+    Date updateAt;
 }
+//new
